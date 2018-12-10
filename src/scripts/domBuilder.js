1a5077d6d25ce1ee3e0fceb006e77300d8bae198
selam <selam0315@gmail.com>
2018-12-10 13:20:43 -0600

    park api fatch

--- src/scripts/domBuilder.js
+++ src/scripts/domBuilder.js
@@ -3,7 +3,7 @@ name: "Dom Builder Component",
 appendInputForm () {
     let displayContainer = document.querySelector("#display-container");
 
-    displayContainer.innerHTML = `
+    displayContainer.innerHTML = (`
     <h1>Welcome to Nashville!</h1>
     <h2>Search for stuff to do today</h2>
     <section id="sectionid">
@@ -25,35 +25,34 @@ appendInputForm () {
     <input type= "text" name="meetups" id="meetups-search" value="concerts by genre">
     <button type="button" id="search-button">Search MeetUps</button>
     </section>
-    `   
-    }
-}
+    `)
+}}
 const myResults= {
-appendResults(){
-    let resultsContainer = document.querySelector("#results-container");
-    resultsContainer.innerHTML= `
-    <div>
-        <h2>Results</h2>
-        <p>address</p>
-    </div> 
-    `
+    appendResults(){
+        let resultsContainer = document.querySelector("#results-container");
+        resultsContainer.innerHTML= `
+        <div>
+            <h2>Results</h2>
+            <p>park_name</p>
+            <p>mapped_location_address</p>
+        </div> 
+        `
+        }
     }
-}
-
-const myItinerary=  {
-appendItenerary(){    
-    let itineraryContainer = document.querySelector("#itinerary-container");
-    itineraryContainer.innerHTML=`
-    <div>
-        <h2>My itinerary</h2>
-        <p>Park</p>
-        <p>Restaurant</p>
-        <p>Meetup</p>
-        <p>Concert</p>
-    </div>`
+    
+    const myItinerary=  {
+    appendItenerary(){    
+        let itineraryContainer = document.querySelector("#itinerary-container");
+        itineraryContainer.innerHTML=`
+        <div>
+            <h2>My itinerary</h2>
+            <p>Park</p>
+            <p>Restaurant</p>
+            <p>Meetup</p>
+            <p>Concert</p>
+        </div>`
+        }
     }
-}
-
-domBuilder.appendInputForm();
-myResults.appendResults();
-myItinerary.appendItenerary();
+    domBuilder.appendInputForm();
+    myResults.appendResults();
+    myItinerary.appendItenerary();
