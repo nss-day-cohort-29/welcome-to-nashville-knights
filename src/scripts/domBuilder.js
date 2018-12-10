bb148c839d0ddf98d8c61be3ae42049d311f430c
Ashwin Prakash <ashp94@gmail.com>
2018-12-10 09:57:21 -0600

    Populated The dom with basic template and containers in index.html

--- src/scripts/domBuilder.js
+++ src/scripts/domBuilder.js
@@ -4,30 +4,56 @@ appendInputForm () {
     let displayContainer = document.querySelector("#display-container");
 
     displayContainer.innerHTML = `
-    <section>
-    <label for="parks">Parks</label>
-    <input type= "text" name="parks" id="parks-search">
+    <h1>Welcome to Nashville!</h1>
+    <h2>Search for stuff to do today</h2>
+    <section id="sectionid">
+    <input type= "text" name="parks" id="parks-search" value="parks by feature">
     <button type="button" id="search-button">Search Parks</button>
     </section>
 
-    <section>
-    <label for="concerts">Concerts</label>
-    <input type= "text" name="concerts" id="concerts-search">
+    <section id="sectionid">
+    <input type= "text" name="concerts" id="concerts-search" value="restaurants by food type">
     <button type="button" id="search-button">Search Concerts</button>
     </section>
 
-    <section>
-    <label for="restaurants">Restaurants</label>
-    <input type= "text" name="restaurants" id="restaurants-search">
+    <section id="sectionid">
+    <input type= "text" name="restaurants" id="restaurants-search" value="meetups by topic">
     <button type="button" id="search-button">Search Restaurants</button>
     </section>
     
-    <section>
-    <label for="meetUps">MeetUps</label>
-    <input type= "text" name="meetups" id="meetups-search">
+    <section id="sectionid">
+    <input type= "text" name="meetups" id="meetups-search" value="concerts by genre">
     <button type="button" id="search-button">Search MeetUps</button>
-    </section>'
+    </section>
     `   
     }
 }
+const myResults= {
+appendResults(){
+    let resultsContainer = document.querySelector("#results-container");
+    resultsContainer.innerHTML= `
+    <div>
+        <h2>Results</h2>
+        <p>address</p>
+    </div> 
+    `
+    }
+}
+
+const myItinerary=  {
+appendItenerary(){    
+    let itineraryContainer = document.querySelector("#itinerary-container");
+    itineraryContainer.innerHTML=`
+    <div>
+        <h2>My itinerary</h2>
+        <p>Park</p>
+        <p>Restaurant</p>
+        <p>Meetup</p>
+        <p>Concert</p>
+    </div>`
+    }
+}
+
 domBuilder.appendInputForm();
+myResults.appendResults();
+myItinerary.appendItenerary();
