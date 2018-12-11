f31d136829291742412e7de24867b3c5422e89e1
selam <selam0315@gmail.com>
2018-12-11 13:50:02 -0600

    updates

--- src/scripts/domComponents.js
+++ src/scripts/domComponents.js
@@ -1,16 +1,2 @@
-let resultsContainer = document.querySelector("#results-container");
-let parkArray = []
-let parkArryObject = {
-    park_name:'',
-    park_address:''  
-}
-  const parkHTML = (park_name, mapped_location_address ) => {
-     let HTMLparkSection = 
-      (`
-      <section class="parkResult">
-        <p>${parkk.park_name}</p>
-        <p>${parkk.mapped_location_address}</p>
-      </section>
-     `)
-   }
+
 
